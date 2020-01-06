7b4eb2d98fc7633ca9ecfa213a346ee22986f189
Natthamon Kharoubi <natthamon.ng@gmail.com>
2020-01-06 22:35:14 +0100

    added forecast list in weather component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Weather from './components/Weather'
 import GetCityForm from './components/GetCityForm'
+import Weather from './components/Weather'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
@@ -14,39 +14,32 @@ class App extends React.Component {
       temp: undefined,
       minTemp: undefined,
       maxTemp: undefined,
-      condition: "",
-      error: false
+      condition: ""
     }
+    this.getWeatherPromise()
   }
 
-  getWeatherPromise = async (e) => {
-    e.preventDefault()
+  getWeatherPromise = async () => {
 
-    let city = e.target.elements.city.value
+    const apiCall = await fetch(`https://www.prevision-meteo.ch/services/json/Toulon`)
+    const weatherData = await apiCall.json()
+    console.log(weatherData)
 
-    if (city) {
-      const apiCall = await fetch(`https://www.prevision-meteo.ch/services/json/${city}`)
-      const response = await apiCall.json()
-      console.log(response)
-
-      this.setState({
-        city: `${response.city_info.name}`,
-        country: response.city_info.country,
-        icon: response.current_condition.icon_big,
-        temp: response.current_condition.tmp,
-        minTemp: response.fcst_day_0.tmin,
-        maxTemp: response.fcst_day_0.tmax,
-        condition: response.current_condition.condition
-      })
-    } else {
-      this.setState({error: true})
-    }
+    this.setState({
+      city: weatherData.city_info.name,
+      country: weatherData.city_info.country,
+      icon: weatherData.current_condition.icon_big,
+      temp: weatherData.current_condition.tmp,
+      minTemp: weatherData.fcst_day_0.tmin,
+      maxTemp: weatherData.fcst_day_0.tmax,
+      condition: weatherData.current_condition.condition
+    })
   }
 
   render() {
     return (
       <div className="App">
-        <GetCityForm loadWeather={this.getWeatherPromise} error={this.state.error} />
+        <GetCityForm />
         <Weather 
           city={this.state.city} 
           country={this.state.country} 
@@ -56,12 +49,15 @@ class App extends React.Component {
           maxTemp={this.state.maxTemp} 
           condition={this.state.condition}
         />
+        
       </div>
     )
   }
 }
 
 export default App
+
+        
 
 
 
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -3,30 +3,84 @@ import React from 'react'
 export default function Weather(props) {
     return (
         <div className="container">
-            <div className="card d-flex align-items-center" style={{borderStyle: 'none'}}>
-                <h1>{props.city}</h1>
-                <div className="py-4">
-                    <img src={props.icon} alt={props.condition} />
+            <div className="row">
+                <div className="col-12 card d-flex align-items-center" style={{borderStyle: 'none'}}>
+                    <h1>{props.city}, {props.country}</h1>
+                    <div className="py-4">
+                        <img src={props.icon} alt={props.condition} />
+                    </div>
+                    <h1 className="py-2">{props.temp}&deg;C</h1>
+
+                    <h3>
+                        <span className="px-4">{props.minTemp}&deg;C</span>
+                        <span className="px-4">{props.maxTemp}&deg;C</span>
+                    </h3>
+
+                    <h4 className="py-3">{props.condition}</h4>                
                 </div>
-                {props.temp ? <h1 className="py-2">{props.temp}&deg;C</h1> : null }
-
-                {/** Show min-max temperature */}
-                {minMaxTemp(props.minTemp, props.maxTemp)}
-
-                <h4 className="py-3">{props.condition}</h4>                
             </div>
+            <div className="row">
+                {getForecastList()}
+            </div>
+            
         </div>
     )
 }
 
-const minMaxTemp = (minTemp, maxTemp) => {
-    if (minTemp && maxTemp) {
+
+const getForecastList = () => {
+    const dayNumbers = [1,2,3,4]
+    for ( let dayNumber of dayNumbers) {
         return (
-            <h3>
-                <span className="px-4">{minTemp}&deg;C</span>
-                <span className="px-4">{maxTemp}&deg;C</span>
-            </h3>
+            <>
+            <div className="col-3 card d-flex align-items-center mt-3" style={{borderStyle: 'none'}}>
+                <p className="py-1">date</p>
+                <div className="py-1">
+                    <img src="https://www.prevision-meteo.ch/style/images/icon/eclaircies.png" alt="" />
+                </div>  
+                <h5>
+                    <span className="px-2">0&deg;C</span>
+                    <span className="px-2">1&deg;C</span>
+                </h5>
+                <p className="py-1">condition</p>               
+            </div>
+
+            <div className="col-3 card d-flex align-items-center mt-3" style={{borderStyle: 'none'}}>
+                <p className="py-1">date</p>
+                <div className="py-1">
+                    <img src="https://www.prevision-meteo.ch/style/images/icon/eclaircies.png" alt="" />
+                </div>  
+                <h5>
+                    <span className="px-2">1&deg;C</span>
+                    <span className="px-2">2&deg;C</span>
+                </h5>
+                <p className="py-1">condition</p>               
+            </div>
+
+            <div className="col-3 card d-flex align-items-center mt-3" style={{borderStyle: 'none'}}>
+                <p className="py-1">date</p>
+                <div className="py-1">
+                    <img src="https://www.prevision-meteo.ch/style/images/icon/eclaircies.png" alt="" />
+                </div>  
+                <h5>
+                    <span className="px-2">2&deg;C</span>
+                    <span className="px-2">3&deg;C</span>
+                </h5>
+                <p className="py-1">condition</p>               
+            </div>
+
+            <div className="col-3 card d-flex align-items-center mt-3" style={{borderStyle: 'none'}}>
+                <p className="py-1">date</p>
+                <div className="py-1">
+                    <img src="https://www.prevision-meteo.ch/style/images/icon/eclaircies.png" alt="" />
+                </div>  
+                <h5>
+                    <span className="px-2">3&deg;C</span>
+                    <span className="px-2">4&deg;C</span>
+                </h5>
+                <p className="py-1">condition</p>               
+            </div>
+        </>
         )
     }
-    
 }
